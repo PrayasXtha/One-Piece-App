305bd41cb3534b2c57d871939cef606b2580bfa1
prayas <prayasshrestha127@gmail.com>
2023-12-08 17:52:12 -0500

    mongoDB

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development', 
   port: process.env.PORT || 3000,
    jwtSecret: process.env.JWT_SECRET || "GROUP_5_KEY", 
-  mongoUri: process.env.MONGODB_URI || "mongodb+srv://v0807:mongo1234@cluster0.h2hfq93.mongodb.net/?retryWrites=true&w=majority"
+  mongoUri: process.env.MONGODB_URI || "mongodb+srv://v0807:mongo1234@cluster0.h2hfq93.mongodb.net/"
   };
-  
+  //mongodb+srv://v0807:mongo1234@cluster0.h2hfq93.mongodb.net/
   export default config
